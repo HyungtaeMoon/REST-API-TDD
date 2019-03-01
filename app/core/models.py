1dc8da2b9793dd16b1e85306d25ffed4659a1bf2
HyungtaeMoon <blessmht@gmail.com>
2019-03-01 23:41:56 +0900

    [ADD] custom user model 추가

---
+++ app/core/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    '''User 에서 사용하기 위한 UserManager 생성'''
+    def create_user(self, email, password=None, **extra_fields):
+        '''일반 유저로 생성할 경우'''
+        if not email:
+            raise ValueError('이메일을 입력해주세요')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        '''superuser 로 user 를 생성할 경우 필드값을 True 로 변경'''
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''UserManager 을 objects 필드에 사용'''
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """이메일을 포함하여 유저가 계정을 생성했을 때 테스트 코드"""
+        email = 'hello@world.com'
+        password = 'test123123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """새로운 유저의 이메일 필드를 표준화(도메인을 소문자로 저장)"""
+        email = 'hello@WORLD.COM'
+        user = get_user_model().objects.create_user(email, 'password123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """유저의 이메일 형식이 올바르지 않을 경우"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'password123')
+
+    def test_new_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'master1@world.com',
+            'password123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
